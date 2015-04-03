8e63dbaf5f499d764c4bfbdabf5add60a42ccbac
Kevin Jing <kevinhigher@gmail.com>
2015-04-03 18:22:07 +0800

    update for index.html

--- src/main/web/www/modules/App2.js
+++ src/main/web/www/modules/App2.js
@@ -1,153 +1,23 @@
-var React = require("react"),
-    Fluxxor = require("fluxxor");
 
-window.React = React;
 
-var constants = {
-    LIST_TODO: "LIST_TODO",
-    DETAIL_TODO: "DETAIL_TODO"
-};
+var React = require('react');
 
-var TodoStore = Fluxxor.createStore({
-    initialize: function() {
-        this.todos = [];
-
-        this.bindActions(
-            constants.ADD_TODO, this.onAddTodo,
-            constants.TOGGLE_TODO, this.onToggleTodo,
-            constants.CLEAR_TODOS, this.onClearTodos
-        );
-    },
-
-    onAddTodo: function(payload) {
-        this.todos.push({text: payload.text, complete: false});
-        this.emit("change");
-    },
-
-    onToggleTodo: function(payload) {
-        payload.todo.complete = !payload.todo.complete;
-        this.emit("change");
-    },
-
-    onClearTodos: function() {
-        this.todos = this.todos.filter(function(todo) {
-            return !todo.complete;
-        });
-        this.emit("change");
-    },
-
-    getState: function() {
-        return {
-            todos: this.todos
-        };
-    }
-});
-
-var actions = {
-    addTodo: function(text) {
-        this.dispatch(constants.ADD_TODO, {text: text});
-    },
-
-    toggleTodo: function(todo) {
-        this.dispatch(constants.TOGGLE_TODO, {todo: todo});
-    },
-
-    clearTodos: function() {
-        this.dispatch(constants.CLEAR_TODOS);
-    }
-};
-
-var stores = {
-    TodoStore: new TodoStore()
-};
-
-var flux = new Fluxxor.Flux(stores, actions);
-
-window.flux = flux;
-
-flux.on("dispatch", function(type, payload) {
-    if (console && console.log) {
-        console.log("[Dispatch]", type, payload);
-    }
-});
-
-var FluxMixin = Fluxxor.FluxMixin(React),
-    StoreWatchMixin = Fluxxor.StoreWatchMixin;
-
-var Application = React.createClass({
-    mixins: [FluxMixin, StoreWatchMixin("TodoStore")],
-
-    getInitialState: function() {
-        return { newTodoText: "" };
-    },
-
-    getStateFromFlux: function() {
-        var flux = this.getFlux();
-        // Our entire state is made up of the TodoStore data. In a larger
-        // application, you will likely return data from multiple stores, e.g.:
-        //
-        //   return {
-        //     todoData: flux.store("TodoStore").getState(),
-        //     userData: flux.store("UserStore").getData(),
-        //     fooBarData: flux.store("FooBarStore").someMoreData()
-        //   };
-        return flux.store("TodoStore").getState();
-    },
-
+var ExampleApplication = React.createClass({
     render: function() {
-        return (
-            <div>
-                <ul>
-          {this.state.todos.map(function(todo, i) {
-              return <li key={i}><TodoItem todo={todo} /></li>;
-          })}
-                </ul>
-                <form onSubmit={this.onSubmitForm}>
-                    <input type="text" size="30" placeholder="New Todo"
-                        value={this.state.newTodoText}
-                        onChange={this.handleTodoTextChange} />
-                    <input type="submit" value="Add Todo" />
-                </form>
-                <button onClick={this.clearCompletedTodos}>Clear Completed</button>
-            </div>
-        );
-    },
+        var elapsed = Math.round(this.props.elapsed  / 100);
+        var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
+        var message =
+            'React has been successfully running for ' + seconds + ' seconds.';
 
-    handleTodoTextChange: function(e) {
-        this.setState({newTodoText: e.target.value});
-    },
-
-    onSubmitForm: function(e) {
-        e.preventDefault();
-        if (this.state.newTodoText.trim()) {
-            this.getFlux().actions.addTodo(this.state.newTodoText);
-            this.setState({newTodoText: ""});
-        }
-    },
-
-    clearCompletedTodos: function(e) {
-        this.getFlux().actions.clearTodos();
+        return <p>{message}</p>;
     }
 });
 
-var TodoItem = React.createClass({
-    mixins: [FluxMixin],
+var start = new Date().getTime();
 
-    propTypes: {
-        todo: React.PropTypes.object.isRequired
-    },
-
-    render: function() {
-        var style = {
-            textDecoration: this.props.todo.complete ? "line-through" : ""
-        };
-
-        return <span style={style} onClick={this.onClick}>{this.props.todo.text}</span>;
-    },
-
-    onClick: function() {
-        this.getFlux().actions.toggleTodo(this.props.todo);
-    }
-});
-
-React.render(<Application flux={flux} />, document.getElementById("app"));
+setInterval(function() {
+    React.render(
+        <ExampleApplication elapsed={new Date().getTime() - start} />,
+        document.getElementById('container')
+    );
+}, 50);
